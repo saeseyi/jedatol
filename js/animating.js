9abef68bf157880e3ffc6643b3c81c556444d80e
saeseyi <oluwasaejoseph@gmail.com>
2022-02-08 00:33:52 +0100

    ,

--- js/animating.js
+++ js/animating.js
@@ -1,3 +1,35 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 5, 2022 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.querySelector(".demo").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.querySelector(".demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+
+
+
 // slideshow picture
 var slideIndex = 0;
 showSlides();
@@ -34,50 +66,8 @@ window.onclick = function(event) {
     }
 }
 
-// menu-bar
-function toggle() {
-    let menu = document.querySelector('.container-1');
-    //if (menu.style.display === "none") {
-    // menu.style.display = "block";
-    // } else {
-    //   menu.style.display = "none";
-    //}
-}
-
 /*if (window.matchMedia("(max-width: 900px)").matches) {
     menu.style.display = "none"
 } else {
     menu.style.display = "block" // Viewport is greater than 700 pixels wide
 }*/
-
-
-
-
-/*/ Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);  */
